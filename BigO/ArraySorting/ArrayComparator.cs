037623d50874ef96ae374ff9c48187977f17d42d
Sinan AKYAZICI <sinan_akyazici@hotmail.com>
2022-06-15 02:09:54 +0300

    ArrayComparator has been added to compare arrays whether they are the same.

---
+++ BigO/ArraySorting/ArrayComparator.cs
@@ -0,0 +1,17 @@
+﻿namespace BigO.ArraySorting;
+
+public class ArrayComparator
+{
+    public static void Compare(params int[][] arrays)
+    {
+        for (var i = 1; i < arrays.Length; i++)
+        {
+            var sequenceEqual = arrays[i-1].SequenceEqual(arrays[i]);
+            if (sequenceEqual == false)
+            {
+                Console.WriteLine("Error: The arrays have been sorted are not the same.");
+                break;
+            }
+        }
+    }
+}
--- BigO/Program.cs
+++ BigO/Program.cs
@@ -1,8 +1,10 @@
 ﻿using BigO.ArraySorting;
 
-const int size = 1000000;
+const int size = 100000;
 var baseArr = ArrayCreator.CreateRandomArray(size);
 
 var arr1 = QuickSort.Run(baseArr);
 var arr2 = MergeSort.Run(baseArr);
 var arr3 = InsertionSort.Run(baseArr);
+
+ArrayComparator.Compare(arr1, arr2, arr3);
