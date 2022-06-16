1f62409ae3ee1bd9f34e5b7d09384f00ef402c6b
Sinan AKYAZICI <sinan_akyazici@hotmail.com>
2022-06-17 01:29:07 +0300

    BubbleSort algorithm has been implemented.

---
+++ BigO/ArraySorting/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿/********************************************************************************************************************************
+* Bubble sort, sometimes referred to as sinking sort                                                                            *
+* Time Complexity Best: O(n), Average: O(n^2), Worst : O(n^2)                                                                   *
+* Space Complexity O(1)                                                                                                         *
+********************************************************************************************************************************/
+
+namespace BigO.ArraySorting;
+
+public class BubbleSort
+{
+    /// <summary>
+    /// The next item of array is selected for comparision.
+    /// </summary>
+    /// <param name="baseArr">The array that will be sorted.</param>
+    public static int[] Run(int[] baseArr)
+    {
+        var arr = (int[])baseArr.Clone();
+        var sw = new Stopwatch();
+        sw.Start();
+        SortArray(arr);
+        sw.Stop();
+        Console.WriteLine($"BubbleSort Elapsed Time : {sw.ElapsedMilliseconds} ms");
+        return arr;
+    }
+
+    private static void SortArray(int[] arr)
+    {
+        var swapFlag = false;
+        for (var j = 1; j < arr.Length - 1; j++)
+        {
+            for (var i = 0; i < arr.Length - j; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+                    swapFlag = true;
+                }
+            }
+
+            if (swapFlag == false) break;
+        }
+    }
+}
--- BigO/Program.cs
+++ BigO/Program.cs
@@ -7,6 +7,7 @@ var arr1 = QuickSort.Run(baseArr);
 var arr2 = MergeSort.Run(baseArr);
 var arr3 = InsertionSort.Run(baseArr);
 var arr4 = SelectionSort.Run(baseArr);
+var arr5 = BubbleSort.Run(baseArr);
 
 Array.Sort(baseArr);
-ArrayComparator.Compare(baseArr, arr1, arr2, arr3, arr4);
+ArrayComparator.Compare(baseArr, arr1, arr2, arr3, arr4, arr5);
