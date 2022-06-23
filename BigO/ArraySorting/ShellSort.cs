a0aba62dda978df0c99f5ac53a6b666cf4e9462d
Sinan AKYAZICI <sinan_akyazici@hotmail.com>
2022-06-23 22:54:45 +0300

    ShellSort algorithm has been implemented.

---
+++ BigO/ArraySorting/ShellSort.cs
@@ -0,0 +1,44 @@
+﻿/********************************************************************************************************************************
+* Shell sort or Shell's method, is an in-place comparison sort. Donald Shell published the first version of this sort in 1959   *
+* Time Complexity Best: O(n*log n), Average: O(n*log n), Worst : O(n^2)                                                         *
+* Space Complexity O(1)                                                                                                         *
+********************************************************************************************************************************/
+
+namespace BigO.ArraySorting;
+
+public class ShellSort
+{
+    /// <summary>
+    /// The original sequence is selected for comparision.
+    /// </summary>
+    /// <param name="baseArr">The array that will be sorted.</param>
+    public static int[] Run(int[] baseArr)
+    {
+        var arr = (int[])baseArr.Clone();
+        var sw = new Stopwatch();
+        sw.Start();
+        SortArray(arr, arr.Length);
+        sw.Stop();
+        Console.WriteLine($"ShellSort Elapsed Time : {sw.ElapsedMilliseconds} ms");
+        return arr;
+    }
+
+    private static void SortArray(int[] arr, int n)
+    {
+        for (var i = n / 2; i > 0; i /= 2)
+        {
+            for (var x = i; x < n; x++)
+            {
+                var temp = arr[x];
+                int y;
+                for (y = x; y >= i && arr[y - i] > temp; y -= i)
+                {
+                    arr[y] = arr[y - i];
+                }
+
+                arr[y] = temp;
+            }
+        }
+
+    }
+}
--- BigO/Program.cs
+++ BigO/Program.cs
@@ -9,6 +9,7 @@ var arr3 = InsertionSort.Run(baseArr);
 var arr4 = SelectionSort.Run(baseArr);
 var arr5 = BubbleSort.Run(baseArr);
 var arr6 = HeapSort.Run(baseArr);
+var arr7 = ShellSort.Run(baseArr);
 
 Array.Sort(baseArr);
-ArrayComparator.Compare(baseArr, arr1, arr2, arr3, arr4, arr5, arr6);
+ArrayComparator.Compare(baseArr, arr1, arr2, arr3, arr4, arr5, arr6, arr7);
